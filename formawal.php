b24b6f77791b7b7fa00288f7e896d9c20cf3f700
restiayumeidianti <43517877+restiayumeidianti@users.noreply.github.com>
2018-09-25 16:56:30 +0700

    Add files via upload

--- formawal.php
+++ formawal.php
@@ -1,5 +1,28 @@
 <?php
-
-
-
-?>
+session_start();
+$username = 'resti';
+$password = 'ayu';
+if (isset($_POST['submit'])) {
+    if ($_POST['username'] == $username && $_POST['password'] == $password){
+        //Membuat Session
+        $_SESSION["username"] = $username; 
+        include 'proses.php';
+        echo "Anda Berhasil Login $username";
+       
+    } else {
+        display_login_form();
+        echo '<p>Username Atau Password Tidak Benar</p>';
+    }
+}    
+else { 
+    display_login_form();
+}
+function display_login_form(){ ?>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method='post'>
+    <label for="username">username</label>
+    <input type="text" name="username" id="username">
+    <label for="password">password</label>
+    <input type="password" name="password" id="password">
+    <input type="submit" name="submit" value="submit">
+    </form>    
+<?php } ?>
--- proses.php
+++ proses.php
@@ -1,4 +1,53 @@
-<?php
+<form action="" method="POST">
+<center><h2 style="font-family: Kristen ITC; text-shadow: 20%">PROFILE</h2></center>
+	<table>
+		<tr>
+			<td>Photo</td>
+			<td> <input type="file" name="photo" style="width: 80px; height: 20px; border-radius: 10%"> </td>
+		</tr>
+		<tr>
+			<td>Hobby</td>
+			<td> : </td>
+		</tr>
+		<tr>
+			<td><input type="checkbox" name="option[]">Acting</td>
+		</tr>
+		<tr>
+			<td><input type="checkbox" name="option[]">Backpacking Travel</td>
+		</tr>
+		<tr>	
+			<td><input type="checkbox" name="option[]">Badminton</td>
+		</tr>
+		<tr>	
+			<td><input type="checkbox" name="option[]">Football</td>
+		</tr>
+		<tr>
+			<td><input type="checkbox" name="option[]">Swimming</td>
+		</tr>
+		<tr>	
+			<td><input type="checkbox" name="option[]">Ballet</td>
+		</tr>
+		<tr>	
+			<td><input type="checkbox" name="option[]">Dancing</td>
+		</tr>	
+		<tr>
+			<td><input type="checkbox" name="option[]">Music</td>
+		</tr>
+		<tr>
+			<td><input type="checkbox" name="option[]">Programming</td>
+		</tr>
+		<tr>
+			<td><input type="checkbox" name="option[]">Other hobby</td>
+		</tr>
 
+	</table>
+
+
+<?php
+	if(isset($_POST['submit'])) {
+		$photo = $_POST['photo'];
+		$hobby =$_POST['hobby'];
+	}
 
 ?>
+</form>
